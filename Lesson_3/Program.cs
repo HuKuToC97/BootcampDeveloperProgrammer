ae6a3bc01b30a19395435dc2bd6e70e5e0c3af08
NiKiTa <hukuta199701@gmail.com>
2023-11-29 13:51:10 +0300

    123

--- Lesson_3/Program.cs
+++ Lesson_3/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿System.Console.Write("Введите число до которого вывести табл. умножения: ");
+int m = Convert.ToInt32(System.Console.ReadLine());
+int[,] matrix = new int[m, m];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        matrix[i, j] = (i + 1) * (j + 1);
+        matrix[j, i] = (i + 1) * (j + 1);
+    }
+}
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < i+1; j++)
+    {
+        System.Console.Write($"{matrix[i, j]}\t");
+    }
+    System.Console.WriteLine();
+}
